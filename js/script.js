c6ad9a36154405b9c9af8f0ba264c90ed3dc5b0d
chaukhadoan <kha.doanchau@gmail.com>
2021-10-17 00:04:31 +0200

    bug fix

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ const headerEl = document.querySelector(".header");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
+  console.log(" i am activated");
 });
 
 //smooth behavior
@@ -20,15 +21,16 @@ allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
     e.preventDefault();
     const href = link.getAttribute("href");
-    if (href === "#")
+    if (href === "#") {
       window.scrollTo({
         top: 0,
         behavior: "smooth",
       });
+    }
 
     if (href !== "#" && href.startsWith("#")) {
       const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: smooth });
+      sectionEl.scrollIntoView({ behavior: "smooth" });
     }
 
     if (link.classList.contains("main-nav-link")) {
